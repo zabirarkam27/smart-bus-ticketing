006e1b8403b6bf29852913dc12df4209469e328e
Zabir Arkam <zabirarkam27@gmail.com>
2024-12-04 20:15:00 +0600

    2nd commit

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const availableSeatEl = document.getElementById("available-seat");
 const inputCouponEl = document.getElementById("input-coupon");
 const btnCouponEl = document.getElementById("btn-coupon");
 const defaultTextEl = document.getElementById("default-text");
+const passengerNameEl = document.getElementById("passenger-name");
+const phoneNumberEl = document.getElementById("phone-number");
+const emailEl = document.getElementById("email");
+const btnNextEl = document.getElementById("btn-next");
+const btnContinue = document.getElementById("btn-continue");
+
 let totalPriceEl = document.getElementById("total-price");
 let grandPriceEl = document.getElementById("grand-price");
-
 let selectedSeat = [];
 let totalPrice = 0;
 
@@ -27,8 +32,7 @@ function handleSelectSeat(event) {
     if (selectedSeat.includes(value)) {
         return alert('This Seat is already booked');
     } else if (selectedSeat.length < 4) {
-        event.classList.add("bg-primary");
-        event.classList.add("text-white");
+        event.classList.add("bg-primary", "text-white");
 
         // booked seat
         selectedSeat.push(event.innerText);
@@ -36,8 +40,7 @@ function handleSelectSeat(event) {
 
         // decrease available seat
         const availableSeatValue = parseFloat(availableSeatEl.innerText);
-        const newAvailableSeatValue = availableSeatValue - 1;
-        availableSeatEl.innerText = newAvailableSeatValue;
+        availableSeatEl.innerText = availableSeatValue - 1;
 
         // remove default text
         defaultTextEl.classList.add('hidden');
@@ -64,18 +67,77 @@ function handleSelectSeat(event) {
 }
 
 // coupon button function
-document.getElementById('btn-coupon').addEventListener('click', function() {
+btnCouponEl.addEventListener('click', function () {
     const couponInputValue = inputCouponEl.value;
+    let couponSave = 0;
 
-    if(couponInputValue === "NEW15"){
-        totalPrice *= 0.85;
-        grandPriceEl.innerText = totalPrice;
-    } else if(couponInputValue === "Couple 20"){
-        totalPrice *= 0.80;
-        grandPriceEl.innerText = totalPrice;
+    // Check if the coupon is valid
+    if (couponInputValue === "NEW15") {
+
+        couponSave = totalPrice * 0.15;
+
+        const couponFormEL = document.getElementById("coupon-form");
+        couponFormEL.innerHTML =
+            `<div class="flex justify-between text-lg font-semibold my-4">
+                <p>Discount</p>
+                <p>
+                    <span>-BDT: </span>
+                    <span>${couponSave.toFixed(2)}</span>
+                </p>
+            </div>`;
+
+        totalPrice = totalPrice - couponSave;
+        grandPriceEl.innerText = totalPrice.toFixed(2);
     }
 
-    else{
+    else if (couponInputValue === "Couple 20") {
+
+        couponSave = totalPrice * 0.20;
+
+        // Hide coupon form 
+        const couponFormEL = document.getElementById("coupon-form");
+        couponFormEL.innerHTML =
+            `<div class="flex justify-between text-lg font-semibold my-4">
+                <p>Discount</p>
+                <p>
+                    <span>-BDT: </span>
+                    <span>${couponSave.toFixed(2)}</span>
+                </p>
+            </div>`;
+
+        totalPrice = totalPrice - couponSave;
+        grandPriceEl.innerText = totalPrice.toFixed(2);
+    }
+    else {
         return alert('You entered wrong coupon');
     }
 })
+
+// Function to enable input fields
+function enableInputFields() {
+    passengerNameEl.removeAttribute("disabled");
+    phoneNumberEl.removeAttribute("disabled");
+    emailEl.removeAttribute("disabled");
+}
+
+function incrementBooked() {
+    const currentValue = parseInt(totalBookedEL.innerText);
+    totalBookedEL.innerText = currentValue; // Update total booked
+    if (parseInt(totalBookedEL.innerText) > 0) {
+        enableInputFields(); // Enable input fields when totalBookedEL > 0
+    }
+}
+
+// enabled next button
+phoneNumberEl.addEventListener('input', function (event) {
+    const inputValue = event.target.value;
+    if (inputValue.length >= 11) {
+        btnNextEl.removeAttribute('disabled');
+    }
+})
+
+// reload the page
+btnContinue.addEventListener('click', function () {
+    window.location.reload();
+})
+
