7a54b2401a3ff702aa40cc022f0ba13c98ae9698
Zabir Arkam <zabirarkam27@gmail.com>
2024-12-04 17:32:48 +0600

    dekho

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const mobileMenuEl = document.getElementById("mobileMenu");
 const selectedSeatEl = document.getElementById("selected-seat");
 const totalBookedEL = document.getElementById("total-booked");
 const availableSeatEl = document.getElementById("available-seat");
-const totalPriceEl = document.getElementById("total-price");
 const inputCouponEl = document.getElementById("input-coupon");
 const btnCouponEl = document.getElementById("btn-coupon");
 const defaultTextEl = document.getElementById("default-text");
+let totalPriceEl = document.getElementById("total-price");
+let grandPriceEl = document.getElementById("grand-price");
+
+let selectedSeat = [];
+let totalPrice = 0;
 
 // menu icons
 // menuBtn.addEventListener('click', function () {
@@ -17,16 +21,12 @@ const defaultTextEl = document.getElementById("default-text");
 //     mobileMenuEl.classList.toggle("flex");
 // })
 
-
-let selectedSeat = [];
-let totalPrice = 0;
-
 function handleSelectSeat(event) {
     const value = event.innerText;
 
     if (selectedSeat.includes(value)) {
         return alert('This Seat is already booked');
-    } else if(selectedSeat.length < 4){
+    } else if (selectedSeat.length < 4) {
         event.classList.add("bg-primary");
         event.classList.add("text-white");
 
@@ -39,10 +39,10 @@ function handleSelectSeat(event) {
         const newAvailableSeatValue = availableSeatValue - 1;
         availableSeatEl.innerText = newAvailableSeatValue;
 
-
         // remove default text
         defaultTextEl.classList.add('hidden');
 
+        // add seats
         selectedSeatEl.innerHTML += `<li class="text-base font-normal flex justify-between">
             <span>${event.innerText}</span>
             <span>Economy</span>
@@ -51,7 +51,6 @@ function handleSelectSeat(event) {
 
         // update total price
         totalPrice += 550;
-        console.log(typeof totalPrice)
         totalPriceEl.innerText = totalPrice.toFixed(2);
 
         // active coupon button
@@ -59,9 +58,24 @@ function handleSelectSeat(event) {
             inputCouponEl.removeAttribute('disabled');
             btnCouponEl.removeAttribute('disabled');
         }
-    }
-    else{
+    } else {
         return alert('You added maximum seats');
     }
-
 }
+
+// coupon button function
+document.getElementById('btn-coupon').addEventListener('click', function() {
+    const couponInputValue = inputCouponEl.value;
+
+    if(couponInputValue === "NEW15"){
+        totalPrice *= 0.85;
+        grandPriceEl.innerText = totalPrice;
+    } else if(couponInputValue === "Couple 20"){
+        totalPrice *= 0.80;
+        grandPriceEl.innerText = totalPrice;
+    }
+
+    else{
+        return alert('You entered wrong coupon');
+    }
+})
